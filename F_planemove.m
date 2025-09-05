8f6f25023007111076326b90c52958260e60ae2d
zMaliu <znaihe@hotmail.com>
2025-09-05 19:01:31 +0800

    Update F_planemove.m

--- F_planemove.m
+++ F_planemove.m
@@ -1,4 +1,8 @@
-function [point]=F_planemove(v,action,pos0,t)
-    action = action / norm(action);
-    point = pos0 + v * action  * t;
+function [pos]=F_planemove(vPlane,dirPlane,pos0,t)
+% vPlane: 速度
+% dirPlane: 水平飞行方向
+% pos0: 初始位置
+% t: 时间
+    dirPlane = dirPlane / norm(dirPlane);
+    pos = pos0 + vPlane * dirPlane  * t;
 end

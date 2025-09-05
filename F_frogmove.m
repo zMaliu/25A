0d23c2203e6e88e54c89c257e41964b1332b7ebf
zMaliu <znaihe@hotmail.com>
2025-09-05 18:57:04 +0800

    Update F_frogmove.m
    frog修改

--- F_frogmove.m
+++ F_frogmove.m
@@ -1,29 +1,27 @@
 % 无人机运动的推广，输入速度 方向 无人机的初始位置 时间 ，输出质点坐标
 % 得t大于某个阈值，我们才会释放烟幕
-% 0-t1 干扰弹跟着无人机做匀速直线运动 t1-t2 干扰弹脱离无人机，做平抛运动
-% t2-t3 干扰弹爆炸，形成烟幕，只有在这个时间段才有输出point
-function [point]=F_frogmove(v,action,pos0,t,t1,t2,param)
-    % 0-t1 匀速直线运动
-    action = action / norm(action);
-    p1 = pos0 + v * action  * t1;
+% 0-tRelease 干扰弹跟着无人机做匀速直线运动 tRelease-tBurst 干扰弹脱离无人机，做平抛运动
+% tBurst-(tBurst+tValid) 干扰弹爆炸，形成烟幕，只有在这个时间段才有输出point
+function [smokePos]=F_frogmove(vPlane,dirPlane,posPlane0,t,tRelease,tBurst,param)
+    % 0-tRelease 匀速直线运动（挂载阶段）
+    dirPlane = dirPlane / norm(dirPlane);
+    posRelease = posPlane0 + vPlane * dirPlane  * tRelease;
 
-    % t1-t2 平抛运动
-    dt = t2-t1;
-    px = p1(1) + v * dt * action(1);
-    py = p1(2) + v * dt * action(2);     
-    pz = p1(3) - 0.5 * param.const.g  * dt^2;
-    p2 = [px py pz]; 
+    % tRelease-tBurst 平抛运动（弹体脱离无人机）
+    dt = tBurst - tRelease;
+    px = posRelease(1) + vPlane * dt * dirPlane(1);
+    py = posRelease(2) + vPlane * dt * dirPlane(2);     
+    pz = posRelease(3) - 0.5 * param.const.g  * dt^2;
+    posBurst = [px py pz]; 
 
-    % t2-t3 干扰弹爆炸
-    t3 = 20;
-    if t>=t2 && t<=t2+t3
-        move_t = t-t2;
-        %pxx = p2(1) + v * move_t * action(1);
-        %pyy = p2(2) + v * move_t * action(2); 
-        pzz = p2(3) - move_t*3;
-        point = [px py pzz];
+    % 起爆后烟幕下沉
+    tValid = param.const.tValid;
+    vDown  = param.const.vSmokeDown;
+    if t>=tBurst && t<=tBurst+tValid
+        move_t = t - tBurst;
+        pzz = posBurst(3) - move_t * vDown;
+        smokePos = [px py pzz];
     else
-        point = [0 0 0]; 
+        smokePos = [0 0 0]; 
     end
-    
 end

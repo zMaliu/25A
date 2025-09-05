31a46192cb19d52d825dae158557e9161f7ec05c
zMaliu <znaihe@hotmail.com>
2025-09-05 19:00:37 +0800

    Update F_missilemove.m

--- F_missilemove.m
+++ F_missilemove.m
@@ -1,4 +1,8 @@
-function [point]=F_missilemove(v,action,pos0,t)
-    action = action / norm(action);
-    point = pos0 + v * action  * t;
+function [pos]=F_missilemove(vMissile,dirMissile,pos0,t)
+% vMissile: 速度
+% dirMissile: 飞行方向
+% pos0: 初始位置
+% t: 时间
+    dirMissile = dirMissile / norm(dirMissile);
+    pos = pos0 + vMissile * dirMissile  * t;
 end

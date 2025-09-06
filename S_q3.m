3d6ad668028d97e39ceb4da9b3cd7805fa5430ff
zMaliu <znaihe@hotmail.com>
2025-09-06 17:36:58 +0800

    Update S_q3.m

--- S_q3.m
+++ S_q3.m
@@ -1,1 +1,17 @@
+%1、找到每时刻t对应的最优点 组成点集
+points=[];%存储最优点
+for t=0:0.1:13
 
+    %进行t时刻可能的解进行遍历 
+    for 
+        %1.1、判断是否可达
+        F_canarrived();
+        %1.2、判断是否遮挡
+        F_isarrived();
+        %1.3、得到t时刻的点集合 进行最优点筛选
+        best_point=F_select();
+    end
+    points=[dots,best_points];%加入最优解
+end
+
+%2、

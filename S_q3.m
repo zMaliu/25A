fd6ecdeb1339b0133e65566e50510d926f102789
kuixin1 <2229697964@qq.com>
2025-09-07 15:49:15 +0800

    1

--- S_q3.m
+++ S_q3.m
@@ -14,9 +14,9 @@ bestT1 = 0;
 bestT2 = 0;
 
 %v_fly=70;angle=180;
-for v_fly=70:1:140
+for v_fly=70:5:140
     for angle=175:1:180
-        for t = 1:0.1:13
+        for t = 1:1:13
             dir_fly = [cosd(angle), sind(angle), 0];
             [pos_fly] = F_planemove(v_fly, dir_fly, pos0_fly, t);
             [pos_missile]=F_missilemove(v_missile,dir_missile,pos0_missile,t);
@@ -30,7 +30,7 @@ for v_fly=70:1:140
             dist = @(P,A,B) norm(cross(P-A, B-A)) / norm(B-A);
         
             for i = 1:96
-                burst_point = [pos_fly(1), pos_fly(2), pos_fly(3) - i * 10];
+                burst_point = [pos_fly(1), pos_fly(2), pos_fly(3) - i*10];
                 dist_line = dist(burst_point, pos_missile, oin);
         
                 if dist_line < min_dist
@@ -39,41 +39,79 @@ for v_fly=70:1:140
                 end
             end
         
-            step=round((t - 1) * 10) + 1;
-            best_points(step).point = best_point;
-            best_points(step).distance = min_dist;
+            %step=round((t - 1) * 10) + 1;
+            best_points(t).point = best_point;
+            best_points(t).distance = min_dist;
 
         end
         % 然后将 best_points(t).distance 进行从小到大排序，选取前三个
         % 提取所有距离
         all_distances = [best_points.distance];
-        all_times = 1:0.1:13;
+        all_times = 1:13;
         
-        % 排序并获取前三个的索引
-        [sorted_dist, idx] = sort(all_distances);
-
-        top3_idx = idx(1:3);
-        top3_times = all_times(idx(1:3));
-
+        % 将距离d2进行排序，然后还有对应的释放时间，然后先找到第一个的释放时间
+        % 然后选取距离第一个点释放时间大于1s的点，这样选取三个点
         points = cell(3, 1);
         posBurst = cell(3,1);
         posRelease = cell(3,1);
         dur = cell(3,1);
         tRelease = cell(3,1);
+        tRelease_temp = cell(13,1);
+
+        [sorted_dist, idx] = sort(all_distances);
+        for i = 1:13
+            sorted_idx = idx(i);
+            points{i} = best_points(sorted_idx).point;
+            [~, ~, ~, tRelease_temp{i}] = F_q3getdur(points{i}, v_fly, angle,pos0_fly,param);
+        end
+        tRelease_one = tRelease_temp{1};
+        
+        % 初始化输出
+        selected_idx = zeros(1, 3);   % 保存选中的原始索引
+        selected_points = cell(1, 3); % 保存选中的 points
+        selected_tRelease = zeros(1, 3); % 保存选中的 tRelease 值
+        
+        % 第一个点
+        selected_tRelease(1) = tRelease_one;
+        [~, first_idx] = ismember(tRelease_one, [tRelease_temp{:}]);
+        selected_idx(1)      = idx(first_idx);
+        selected_points{1}   = points{first_idx};
+
+        % 新增：已选掩码 
+        already_picked = false(13,1);          % 13 是总长度
+        already_picked(first_idx) = true;      % 把第一个点标记为已选
+        
+        % 第二个点 
+        diff1 = abs([tRelease_temp{:}] - tRelease_one);
+        diff1(already_picked) = NaN;            % 排除已选
+        valid1 = find(diff1 >= 1, 1, 'first');
+        selected_tRelease(2) = tRelease_temp{valid1};
+        selected_idx(2)      = idx(valid1);
+        selected_points{2}   = points{valid1};
+        already_picked(valid1) = true;          % 标记
+        
+        % 第三个点 
+        tRelease_two = selected_tRelease(2);
+        diff2 = abs([tRelease_temp{:}] - tRelease_two);
+        diff2(already_picked) = NaN;            % 排除已选
+        valid2 = find(diff2 >= 1, 1, 'first');
+        selected_tRelease(3) = tRelease_temp{valid2};
+        selected_idx(3)      = idx(valid2);
+        selected_points{3}   = points{valid2};
 
         for i = 1:3
-            idx = top3_idx(i);
-            points{i} = best_points(idx).point;
+            sorted_idx = selected_idx(i);
+            points{i} = best_points(sorted_idx).point;
             [posBurst{i}, posRelease{i}, dur{i}, tRelease{i}] = F_q3getdur(points{i}, v_fly, angle,pos0_fly,param);
-
         end
         dur_vals = [dur{:}];  
-        tRelease_vals = [tRelease{:}];  
 
         % 构造结果
         result = cell(1,3);
+
         for i = 1:3
-            result{i} = [top3_times(i), dur_vals(i) + top3_times(i)];
+            sorted_idx = selected_idx(i);
+            result{i} = [all_times(sorted_idx), dur_vals(i) + all_times(sorted_idx)];
         end
 
         [dur_end, bestPick, unionInt] = F_q4select(result);
--- S_q4.m
+++ S_q4.m
@@ -9,91 +9,40 @@ posFake=param.target.posFake;
 pos0_missile=param.missiles(1).pos0;
 v_missile=param.const.missileV;
 dir_missile=posFake - pos0_missile;
-
 % 用于存储每个 t 时刻的最优点的结构
-best_points = struct('point', [], 'distance', []);
+best_points1 = struct('point', [], 'distance', []);
 
 maxDur = -inf;
 bestV = 0;
 bestT1 = 0;
 bestT2 = 0;
 
-v_fly1=70;v_fly2=80;v_fly3=80;angle_fly1=180;angle_fly2=180;angle_fly3=135;
+v_fly1=70;v_fly2=80;v_fly3=80;angle_fly1=180;angle_fly2=175;angle_fly3=135;
 % 固定三架无人机的速度和航向角
-%{
-for v_fly1=70:5:140
-    for v_fly2=70:5:140  
-        for v_fly3=70:5:140
+
+for v_fly1=70:10:140
+    for v_fly2=70:10:140  
+        for v_fly3=70:10:140
             for angle_fly1=175:1:180
-                for angle_fly2=180:1:270
-                    for angle_fly3=135:1:180
+                for angle_fly2=180:5:270
+                    for angle_fly3=135:5:180
+                        [result1,~,~,~]=F_q4_3points(angle_fly1,v_fly1,pos0_fly1,param);
+                        [result2,~,~,~]=F_q4_3points(angle_fly2,v_fly2,pos0_fly2,param);
+                        [result3,~,~,~]=F_q4_3points(angle_fly3,v_fly3,pos0_fly3,param);
+                        result=cell(1,3);
+                        result{1}=result1;
+                        result{2}=result2;
+                        result{3}=result3;
+                        
+                        [dur_end, ~, ~] = F_q4select(result);
+                        if dur_end > maxDur
+                            maxDur = dur_end;
+                        end
+                        fprintf('\n最优结果:\n');
+                        fprintf('最大遮蔽时长: %.1f \n', maxDur);
                     end
                 end
             end
         end
     end
-end
-%}
-v_fly      = [v_fly1; v_fly2; v_fly3];        % 3×1  速度
-angle_fly  = [angle_fly1; angle_fly2; angle_fly3];  % 3×1  角度
-pos0_fly   = [pos0_fly1; pos0_fly2; pos0_fly3];     % 3×1  初始位置
-
-% 先计算一架飞机
-for j=1:3
-    for t = 1:13
-        dir_fly = [cosd(angle_fly(j)), sind(angle_fly(j)), 0];
-        [pos_fly] = F_planemove(v_fly(j), dir_fly, pos0_fly(j), t);  
-        [pos_missile]=F_missilemove(v_missile,dir_missile,pos0_missile,t);
-    
-        min_dist = inf;
-        best_point = [];
-    
-        % d2的计算
-        odown = param.target.posTrue;      
-        oin   = odown + [0 0  5];
-        dist = @(P,A,B) norm(cross(P-A, B-A)) / norm(B-A);
-    
-        % 先拟定h高度最大是1800
-        for i = 1:180
-            burst_point = [pos_fly(1), pos_fly(2), pos_fly(3) - i * 10];
-            dist_line = dist(burst_point, pos_missile, oin);
-    
-            if dist_line < min_dist
-                min_dist = dist_line;
-                best_point = burst_point;
-            end
-        end 
-        best_points(t).point = best_point;
-        best_points(t).distance = min_dist;
-
-    end
-        
-    % 然后将 best_points(t).distance 进行从小到大排序，选取前三个
-    % 提取所有距离
-    all_distances = [best_points.distance];
-    all_times = 1:13;
-        
-    % 排序并获取前三个的索引
-    [sorted_dist, idx] = sort(all_distances);
-    
-    top3_idx = idx(1:3);
-    top3_times = all_times(idx(1:3));
-    
-    points = cell(3, 1);
-    posBurst = cell(3,1);
-    posRelease = cell(3,1);
-    dur = cell(3,1);
-    tRelease = cell(3,1);
-    for i = 1:3
-        idx = top3_idx(i);
-        points{i} = best_points(idx).point;
-        [posBurst{i}, posRelease{i}, dur{i}, tRelease{i}] = F_q3getdur(points{i}, v_fly, angle_fly(j),pos0_fly,param);
-    
-    end
-    dur_vals = [dur{:}];  
-    
-    % 构造结果
-    result = cell(1,3);
-    result{j} = [top3_times(:), top3_times(:) + dur_vals(:)];
-    
 end
